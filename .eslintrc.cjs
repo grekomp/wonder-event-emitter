44ea6da3992359ad2bdc9aaa4cfe87ef6edd3de2
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-04-22 00:04:16 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+/** @type { import("eslint").Linter.Config } */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  overrides: [],
+  root: true,
+};
---
+++ src/hello.test.ts
@@ -0,0 +1,14 @@
+// import assert from "node:assert/strict";
+// import { describe, test } from "node:test";
+// import { returnHelloWorld } from "./hello";
+
+// describe("unit tests and code side by side", () => {
+//   test("returnHelloWorld() - returns 'hello world'", () => {
+//     console.log("testing returnHelloWorld()");
+//     assert.equal(returnHelloWorld(), "hello world");
+//     console.log("Done");
+
+//     const k = [1, 2, 3];
+//     delete k[1];
+//   });
+// });
