055a81fd9468bb8a0f67802100d245443cf9f1b4
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-04-28 22:10:43 +0200

    Added bound event functions and types to entrypoint index file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import {
+  bindEvent,
+  bindEventDictionary,
+} from "src/event-emitter/bound-event-descriptors";
+import {
   defineEvent,
   defineEventDictionary,
 } from "src/event-emitter/event-descriptors";
 import { EventEmitter } from "src/event-emitter/event-emitter";
 
+export * from "src/event-emitter/bound-event-descriptors.model";
 export * from "src/event-emitter/event-descriptors.model";
 export * from "src/event-emitter/event-emitter.model";
 export {
   EventEmitter as WonderEventEmitter,
+  bindEvent,
+  bindEventDictionary,
   defineEvent,
   defineEventDictionary,
 };
